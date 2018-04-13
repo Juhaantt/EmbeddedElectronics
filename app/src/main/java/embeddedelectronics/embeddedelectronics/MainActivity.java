dcf3181653d750bfce2a3fe5cf901339b09b7afb
Juha Anttila <jhsant@utu.fi>
2018-04-13 16:41:59 +0300

    Bluetooth connection and sensor data

--- app/src/main/java/embeddedelectronics/embeddedelectronics/MainActivity.java
+++ app/src/main/java/embeddedelectronics/embeddedelectronics/MainActivity.java
@@ -1,13 +1,161 @@
 package embeddedelectronics.embeddedelectronics;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
+import java.io.InputStream;
+import android.content.Intent;
+import android.widget.TextView;
+import java.io.OutputStream;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView alcohol_level;
+    ProgressBar alcogol_level_visual;
+
+    private final String DEVICE_ADDRESS="20:13:10:15:33:66";
+    private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");//Serial Port Service ID
+    private BluetoothDevice device;
+    private BluetoothSocket socket;
+    private OutputStream outputStream;
+    private InputStream inputStream;
+    boolean deviceConnected=false;
+    Thread thread;
+    byte buffer[];
+    int bufferPosition;
+    boolean stopThread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        alcohol_level = (TextView) findViewById(R.id.alcohol_level);
+        alcogol_level_visual = (ProgressBar) findViewById(R.id.alcohol_level_visual);
+        setUiEnabled(false);
+
+        BTinit();
+        BTconnect();
+
+    }
+
+    public void setUiEnabled(boolean bool)
+    {
+        alcohol_level.setEnabled(bool);
+        alcogol_level_visual.setEnabled(bool);
+    }
+
+    public boolean BTinit()
+    {
+        boolean found=false;
+        BluetoothAdapter bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(),"Device doesnt Support Bluetooth",Toast.LENGTH_SHORT).show();
+        }
+        if(!bluetoothAdapter.isEnabled())
+        {
+            Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableAdapter, 0);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
+        if(bondedDevices.isEmpty())
+        {
+            Toast.makeText(getApplicationContext(),"Please Pair the Device first",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            for (BluetoothDevice iterator : bondedDevices)
+            {
+                if(iterator.getAddress().equals(DEVICE_ADDRESS))
+                {
+                    device=iterator;
+                    found=true;
+                    break;
+                }
+            }
+        }
+        return found;
+    }
+
+    public boolean BTconnect()
+    {
+        boolean connected=true;
+        try {
+            socket = device.createRfcommSocketToServiceRecord(PORT_UUID);
+            socket.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            connected=false;
+        }
+        if(connected)
+        {
+            try {
+                outputStream=socket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                inputStream=socket.getInputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            setUiEnabled(true);
+            deviceConnected=true;
+            beginListenForData();
+        }
+
+
+        return connected;
+    }
+
+    void beginListenForData()
+    {
+        final android.os.Handler handler = new android.os.Handler();
+        stopThread = false;
+        buffer = new byte[1024];
+        Thread thread  = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopThread)
+                {
+                    try
+                    {
+                        int byteCount = inputStream.available();
+                        if(byteCount > 0)
+                        {
+                            byte[] rawBytes = new byte[byteCount];
+                            inputStream.read(rawBytes);
+                            final String string=new String(rawBytes,"UTF-8");
+                            handler.post(new Runnable() {
+                                public void run()
+                                {
+                                    alcohol_level.append(string);
+                                }
+                            });
+
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        stopThread = true;
+                    }
+                }
+            }
+        });
+
+        thread.start();
     }
 }
